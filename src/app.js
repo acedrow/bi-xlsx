6438a5c12b98b8f8b9974a741cbccf73283ef2c5
acedrow <ace.holt@gmail.com>
2023-09-05 21:51:30 -0500

    add sheets, JSON support

--- src/app.js
+++ src/app.js
@@ -1,14 +1,33 @@
-const http = require('http');
- 
-const hostname = '127.0.0.1';
+const http = require("http");
+var XLSX = require("xlsx");
+
+const hostname = "127.0.0.1";
 const port = 3000;
- 
+
+const headerRow =
+  "Team,T,Fw,Fi,F,Cfw,F/T,Rw,Rd,Rl,R,Crw,Sw,Sw/r,Sl,Sl/r,Sdif,Ce,YK,RK, ";
+
+const testSecondRow = "TeamName,,,,=D3/F3";
+const startingDataRow = 3;
+
+var data = [
+  { A: "Title", B: "b1" },
+  { A: "URL", B: "b2" },
+];
+
+//expects teamnames as an array of strings
+const buildSpreadsheetRows = (teamnames) => {};
+
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello poopppp');
+
+  var ws = XLSX.utils.json_to_sheet(data, { skipHeader: true });
+  var wb = XLSX.utils.book_new();
+  XLSX.utils.book_append_sheet(wb, ws, "sheet1");
+  res.setHeader('Content-Disposition', 'attachment; filename="SheetJS.xlsx"');
+  res.end(XLSX.write(wb, {type:"buffer", bookType: "xlsx"}));
 });
- 
+
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
