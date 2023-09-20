cc3548f30b66e24c7e869831c6a8912f32c7946e
acedrow <ace.holt@gmail.com>
2023-09-19 21:19:58 -0500

    add row merges, actual header data

--- src/app.js
+++ src/app.js
@@ -3,29 +3,107 @@ var XLSX = require("xlsx");
 
 const hostname = "127.0.0.1";
 const port = 3000;
+const DATA_START_ROW = 3
 
-const headerRow =
-  "Team,T,Fw,Fi,F,Cfw,F/T,Rw,Rd,Rl,R,Crw,Sw,Sw/r,Sl,Sl/r,Sdif,Ce,YK,RK, ";
+const row0HeaderCells = {
+  A: "",
+  B: "Team",
+  C: "T",
+  D: "Fights",
+  E: "",
+  F: "",
+  G: "",
+  H: "",
+  I: "Rounds",
+  J: "",
+  K: "",
+  L: "",
+  M: "",
+  N: "Score",
+  O: "",
+  P: "",
+  Q: "",
+  R: "",
+  S: "",
+  T: "Cards",
+  U: "",
+  V: "Points",
+};
 
-const testSecondRow = "TeamName,,,,=D3/F3";
-const startingDataRow = 3;
+const row1HeaderCells = {
+  A: "",
+  B: "",
+  C: "T",
+  D: "Fw",
+  E: "Fl",
+  F: "F",
+  G: "Cfw",
+  H: "F/T",
+  I: "Rw",
+  J: "Rd",
+  K: "Rl",
+  L: "R",
+  M: "Crw",
+  N: "Sw",
+  O: "Sw/r",
+  P: "Sl",
+  Q: "Sl/r",
+  R: "Sdif",
+  S: "Ce",
+  T: "YK",
+  U: "RK",
+  V: "",
+};
 
-var data = [
-  { A: "Title", B: "b1" },
-  { A: "URL", B: "b2" },
+const headerMerges = [
+  { s: { r: 0, c: 1 }, e: { r: 1, c: 1 } },
+  { s: { r: 0, c: 2 }, e: { r: 1, c: 2 } },
+  { s: { r: 0, c: 3 }, e: { r: 0, c: 7 } },
+  { s: { r: 0, c: 8 }, e: { r: 0, c: 12 } },
+  { s: { r: 0, c: 13 }, e: { r: 0, c: 18 } },
+  { s: { r: 0, c: 19 }, e: { r: 0, c: 20 } },
+  { s: { r: 0, c: 21 }, e: { r: 1, c: 21 } },
 ];
 
 //expects teamnames as an array of strings
-const buildSpreadsheetRows = (teamnames) => {};
+const buildTeamRows = (teamnames) => {
+  teamnames.map((team, i) => ({
+    A: i,
+    B: team,
+    C: 1,
+    D: 0,
+    E: 0,
+    F: 0,
+    G: `=D3/F3`,
+    H: "F/T",
+    I: "Rw",
+    J: "Rd",
+    K: "Rl",
+    L: "R",
+    M: "Crw",
+    N: "Sw",
+    O: "Sw/r",
+    P: "Sl",
+    Q: "Sl/r",
+    R: "Sdif",
+    S: "Ce",
+    T: "YK",
+    U: "RK",
+    V: "",
+  }));
+};
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
 
-  var ws = XLSX.utils.json_to_sheet(data, { skipHeader: true });
+  var ws = XLSX.utils.json_to_sheet([row0HeaderCells, row1HeaderCells], {
+    skipHeader: true,
+  });
+  ws["!merges"] = [...headerMerges];
   var wb = XLSX.utils.book_new();
   XLSX.utils.book_append_sheet(wb, ws, "sheet1");
-  res.setHeader('Content-Disposition', 'attachment; filename="SheetJS.xlsx"');
-  res.end(XLSX.write(wb, {type:"buffer", bookType: "xlsx"}));
+  res.setHeader("Content-Disposition", 'attachment; filename="SheetJS.xlsx"');
+  res.end(XLSX.write(wb, { type: "buffer", bookType: "xlsx" }));
 });
 
 server.listen(port, hostname, () => {
