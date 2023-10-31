b9c57da89a47c5e2fe9aa02c9b7330457f8e66c6
Linden Holt <lindenholt@Lindens-MacBook-Pro-2.local>
2023-10-31 12:30:31 -0500

    change file structure, add column widths

--- src/app.js
+++ src/app.js
@@ -1,45 +1,11 @@
 import tempfile from 'tempfile'
-import ExcelJS from 'exceljs'
 import express from 'express'
+import generateXlsx from './sheetGeneration/generateSheet.js'
 const port = process.env.PORT || 8080
 const app = express()
 
-const initWorkbook = () => {
-  const workbook = new ExcelJS.Workbook()
-  const worksheet = workbook.addWorksheet('My Sheet')
-
-  worksheet.columns = [
-    { key: 'A', width: 10 },
-    { key: 'B', width: 32 },
-    { key: 'C', width: 10 }
-  ]
-
-  const row0 = worksheet.addRow({ A: 'A!', C: 'C!' })
-  row0.fill = {
-    type: 'pattern',
-    pattern: 'solid',
-    fgColor: { argb: 'ff4ccf70' }
-  }
-  row0.font = {
-    name: 'Arial Black',
-    color: { argb: '00000000' },
-    family: 2,
-    size: 14,
-    italic: false
-  }
-  const row1 = worksheet.addRow({ A: 'B should be dropdown', C: 'C!' })
-
-  worksheet.getCell('B2').dataValidation = {
-    type: 'list',
-    allowBlank: true,
-    formulae: ['One,Two,Three,Forr']
-  }
-
-  return workbook
-}
-
 app.get('/', (req, res) => {
-  const workbook = initWorkbook()
+  const workbook = generateXlsx()
   res.statusCode = 200
   const tempFilePath = tempfile({ extension: '.xlsx' })
   workbook.xlsx.writeFile(tempFilePath).then(function () {
---
+++ src/sheetGeneration/sheetConstants.js
@@ -0,0 +1,38 @@
+const COL_SIZE = {
+  xs: 4,
+  s: 6,
+  m: 10,
+  l: 16,
+  xl: 24
+}
+
+const WORKSHEET_COLS = [
+  { key: 'A', width: COL_SIZE.l },
+  { key: 'B', width: COL_SIZE.xl },
+  { key: 'C', width: COL_SIZE.s },
+  { key: 'D', width: COL_SIZE.xs },
+  { key: 'E', width: COL_SIZE.xs },
+  { key: 'F', width: COL_SIZE.xs },
+  { key: 'G', width: COL_SIZE.xs },
+  { key: 'H', width: COL_SIZE.xs },
+  { key: 'I', width: COL_SIZE.xs },
+  { key: 'J', width: COL_SIZE.xs },
+  { key: 'K', width: COL_SIZE.xs },
+  { key: 'L', width: COL_SIZE.xs },
+  { key: 'M', width: COL_SIZE.xs },
+  { key: 'N', width: COL_SIZE.xs },
+  { key: 'O', width: COL_SIZE.xs },
+  { key: 'P', width: COL_SIZE.xs },
+  { key: 'Q', width: COL_SIZE.xs },
+  { key: 'R', width: COL_SIZE.xs },
+  { key: 'S', width: COL_SIZE.xs },
+  { key: 'T', width: COL_SIZE.s },
+  { key: 'U', width: COL_SIZE.s },
+  { key: 'V', width: COL_SIZE.s },
+  { key: 'W', width: COL_SIZE.m },
+  { key: 'X', width: COL_SIZE.m },
+  { key: 'Y', width: COL_SIZE.m },
+  { key: 'Z', width: COL_SIZE.l }
+]
+
+export { WORKSHEET_COLS }
