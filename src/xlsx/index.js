6dfda176125f712e00961e7f93e03a84e3704ba4
Linden Holt <ace.holt@gmail.com>
2023-11-14 08:56:51 -0600

    change teams -> competitors, remove xlsx file

--- src/xlsx/index.js
+++ src/xlsx/index.js
@@ -3,11 +3,12 @@ import generateResultsSheet from './generateResultsSheet.js'
 import generatePoolsSheet from './generatePoolsSheet.js'
 import generateBracketsSheet from './generateBracketsSheet.js'
 
-const generateXlsx = ({ eventName, date, location, teams }) => {
+const generateXlsx = ({ eventName, date, location, teams, fighters }) => {
   const workbook = new ExcelJS.Workbook()
-  generateResultsSheet(workbook, { eventName, date, location, teams })
-  generatePoolsSheet(workbook, { eventName, date, location, teams })
-  generateBracketsSheet(workbook, { eventName, date, location, teams })
+  const competitors = teams || fighters
+  generateResultsSheet(workbook, { eventName, date, location, teams: competitors })
+  generatePoolsSheet(workbook, { eventName, date, location, teams: competitors })
+  generateBracketsSheet(workbook, { eventName, date, location, teams: competitors })
   return workbook
 }
 
